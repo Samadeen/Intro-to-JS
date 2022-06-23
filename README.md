201e235f65890e0dc05f569a3fdc8e55fb4cfbaa
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-06-23 21:45:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Intro-to-JS
+ Zuri Task to alert info on screen with JS
---
+++ script.js
@@ -0,0 +1,9 @@
+const info = {
+  name: 'Abdul',
+  height: `6'3`,
+  country: 'Nigeria',
+};
+
+alert(
+  `Hi, my Name is ${info.name}, I'm ${info.height} tall and I'm from ${info.country}`
+);
